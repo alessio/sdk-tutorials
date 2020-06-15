6ef263d58334207a7566649689b4823913b67914
Alessio Treglia <alessio@tendermint.com>
2020-06-15 12:02:13 +0200

    new request

--- nameservice/example_request.curl.sh
+++ nameservice/example_request.curl.sh
@@ -1,2 +1,4 @@
-curl -H 'Content-Type: application/json' --request POST --data @example_request http://127.0.0.1:1317/bank/accounts/cosmos1hdv8nudklzq9fk9xr2uez5a3rqq2add5gamj49/transfers
-{"error":"http: request body too large"}
+#curl -H 'Content-Type: application/json' --request POST --data @example_request http://127.0.0.1:1317/bank/accounts/cosmos1hdv8nudklzq9fk9xr2uez5a3rqq2add5gamj49/transfers
+curl -H 'Content-Type: application/json' --request POST --data @example_request http://127.0.0.1:1317/nameservice/names
+
+# {"error":"http: request body too large"}
