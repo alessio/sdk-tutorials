c6b33527ef598ef687b85477dfc4ce0cc39b6e31
Alessio Treglia <alessio@tendermint.com>
2020-06-15 12:36:38 +0200

    import keys from seeds

--- nameservice/init.sh
+++ nameservice/init.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+shopt -s expand_aliases
+
+alias nsd=$(pwd)/nsd
+alias nscli=$(pwd)/nscli
+
 rm -rf ~/.nsd
 rm -rf ~/.nscli
 
@@ -11,10 +18,15 @@ nscli config trust-node true
 nscli config chain-id namechain
 nscli config keyring-backend test
 
-nscli keys add jack
-nscli keys add alice
-nscli keys add foo
-nscli keys add bar
+jackseed='sport eternal despair major raccoon arrest zoo magic measure ensure relief multiply priority paper reveal scrap know word lawn wagon elbow trumpet unveil young'
+aliceseed='fork digital rare coin permit couch business energy session arm icon profit rule peasant ability walk office open hawk churn special over excite vessel'
+fooseed='laugh circle useless shift maze quantum ribbon auction check pledge toilet ripple hobby radio indoor nice segment torch crucial ecology omit jaguar aunt later'
+barseed='frown attack build change parade way brand advice voice basket panda marble reduce renew paper suspect arch club ketchup toddler include deal leader float'
+
+nscli keys add --recover jack <<<"${jackseed}"
+nscli keys add --recover alice <<<"${aliceseed}"
+nscli keys add --recover foo <<<"${fooseed}"
+nscli keys add --recover bar <<<"${barseed}"
 
 nsd add-genesis-account $(nscli keys show jack -a) 1000nametoken,100000000stake
 nsd add-genesis-account $(nscli keys show alice -a) 1000nametoken,100000000stake
@@ -28,3 +40,7 @@ nsd collect-gentxs
 
 echo "Validating genesis file..."
 nsd validate-genesis
+
+for keyname in jack alice foo bar; do
+  nscli keys show --address $keyname
+done
