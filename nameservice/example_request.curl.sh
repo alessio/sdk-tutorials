e3b8e6d928568613e5dfc4f51793eeefcfc61815
Alessio Treglia <alessio@tendermint.com>
2020-06-15 11:15:31 +0200

    Upload example request

---
+++ nameservice/example_request.curl.sh
@@ -0,0 +1,2 @@
+curl -H 'Content-Type: application/json' --request POST --data @example_request http://127.0.0.1:1317/bank/accounts/cosmos1hdv8nudklzq9fk9xr2uez5a3rqq2add5gamj49/transfers
+{"error":"http: request body too large"}
